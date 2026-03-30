26785dc833f30e9379b0481a51dbae504732c8e2
justlele24 <lele.barra2403@gmail.com>
2026-03-30 11:15:12 +0200

    contentview

--- Calmm/ContentView.swift
+++ Calmm/ContentView.swift
@@ -8,28 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 2
+
     var body: some View {
-        TabView {
-            Text("Home")
-                .tabItem {
-                    Label("Home", systemImage: "house.fill")
-                }
+        TabView(selection: $selectedTab) {
             Text("Games")
                 .tabItem {
                     Label("Games", systemImage: "gamecontroller.fill")
                 }
+                .tag(0)
+
             Text("Shop")
                 .tabItem {
                     Label("Shop", systemImage: "bag.fill")
                 }
+                .tag(1)
+
+            HomeView()
+                .tabItem {
+                    Label("Kennel", systemImage: "house.fill")
+                }
+                .tag(2)
+
             Text("Style")
                 .tabItem {
                     Label("Style", systemImage: "tshirt.fill")
                 }
+                .tag(3)
+
             Text("Profile")
                 .tabItem {
                     Label("Profile", systemImage: "star.fill")
                 }
+                .tag(4)
         }
     }
 }
