c00ca54042a8a0f34eedda8c0f8de1508dd397e4
Claudia Napolitano <120132654+cldnpl@users.noreply.github.com>
2026-03-30 09:57:49 +0200

    Initial Commit

---
+++ Calmm/CalmmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalmmApp.swift
+//  Calmm
+//
+//  Created by Claudia Napolitano on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CalmmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
